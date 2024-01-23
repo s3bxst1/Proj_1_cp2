c1803f9e955a47d15d7dc2de63866d35e2015ffe
s3bxst1 <s3bxs@outlook.com>
2024-01-22 23:08:31 -0500

    Proj 1 for CP2

---
+++ src/PersonReader.java
@@ -0,0 +1,13 @@
+//public class PersonReader {
+   // public static void main(String[] args) {
+      //  System.out.println("Hello world!");
+    //}
+//}
+
+//try{
+    //File workingDirectory = new File(System.getPropertu("user.dir"));
+
+    //chooser.setCurrentDirectory(workingDirectory);
+
+    //if(chooser.showOpenDialog())
+        //}
